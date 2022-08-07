d57cd55d41da9325626c64130a5a430efff5057b
Carlos Eduardo Dal Molin <carlosdudu164@gmail.com>
2022-08-06 23:01:15 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,16 +8,22 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private static Scene mainScene;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
         ScrollPane scrollPane = loader.load();
         scrollPane.setFitToHeight(true);
         scrollPane.setFitToWidth(true);
-        Scene mainScene = new Scene(scrollPane);
+        mainScene = new Scene(scrollPane);
         primaryStage.setScene(mainScene);
         primaryStage.setTitle("Sample JavaFX application");
         primaryStage.show();
+    }
+
+    public static Scene getMainScene() {
+        return mainScene;
     }
 
     public static void main(String[] args) {
